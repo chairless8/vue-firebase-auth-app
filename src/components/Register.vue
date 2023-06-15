ea485bcb094c26e3ed00285dd4a66622a544e457
jesen248 <jesenvt@sisidev.com>
2023-06-15 06:39:10 -0700

    Dime algo

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,23 +1,39 @@
 <template>
-<div>
-<h2>Register</h2>
-<!-- Aquy puedes agregar tu formulario de registro -->
-</div>
-</template>
-
-<script>
-const firebase = require('firebase');
-export default {
-  name: 'Register',
-  data() {
-    return {
-      // Aquí establece tus datos de registro
+  <div>
+  <h2>Register</h2>
+  <form v-on:submit="handleRegister">
+    <label>Email:</label>
+    <input v-model="email" type="email" required>
+    <label>Password:</label>
+    <input v-model="password" type="password" required>
+    <button type="submit">Register</button>
+  </form>
+  </div>
+  </template>
+  
+  <script>
+  import firebase from 'firebase/app'
+  import 'firebase/auth'
+  
+  export default {
+    name: 'Register_firebase',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async handleRegister() {
+        try {
+          await firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
     }
-  },
-  methods: {
-    // Aquí establece tus metodos de registro
   }
-}
-</script>
-
-<style scoped></style>
+  </script>
+  
+  <style scoped></style>
+  
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// In your main.js file
+import { createApp } from 'vue'
+import App from './App.vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+var firebaseConfig = {
+  // your-config
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 createApp(App).mount('#app')
+
