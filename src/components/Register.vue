5797bfcb278aa023c4c21e3d93aabc87400eb720
chairless8 <136710429+chairless8@users.noreply.github.com>
2023-06-15 06:15:03 -0700

    Update Register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<h2>Register</h2>
+<!-- Aquy puedes agregar tu formulario de registro -->
+</div>
+</template>
+
+<script>
+const firebase = require('firebase');
+export default {
+  name: 'Register',
+  data() {
+    return {
+      // Aquí establece tus datos de registro
+    }
+  },
+  methods: {
+    // Aquí establece tus metodos de registro
+  }
+}
+</script>
+
+<style scoped></style>
