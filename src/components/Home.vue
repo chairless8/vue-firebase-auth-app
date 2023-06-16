0fceec7d700df83cff4de48600a453923b03f242
jesen248 <jesenvt@sisidev.com>
2023-06-16 06:52:35 -0700

    basic structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,82 @@
 <template>
-    <div>
-    <h2>Home</h2>
-    <p>Welcome, {{ user?.email }}</p>
-    </div>
+  <div class="container">
+      <!-- Navigation Bar -->
+      <nav>
+      <h2>My App</h2>
+      <p v-if="user">Welcome, {{ user.email }}</p>
+      <button v-if="user" v-on:click="handleLogout">Logout</button>
+      <router-link v-else to="/login">Login</router-link>
+      </nav>
+      
+    <!-- Content -->
+    <main>
+      <h2>Home</h2>
+      <p>This is the main content of the page.</p>
+      <GridNavigation />
+    </main>
+
+    <!-- Footer -->
+    <footer>
+      <p>© 2023 My App. All rights reserved.</p>
+    </footer>
+  </div>
 </template>
-    
+
 <script>
 import { ref } from 'vue';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import GridNavigation from './MainGrid.vue'
 
 export default {
-    name: 'Home_firebase',
-    setup() {
-    const user = ref(null);
-    const auth = getAuth();
-    onAuthStateChanged(auth, (currentUser) => {
-        user.value = currentUser;
-    });
-    return { user };
+name: 'Home_firebsae',
+components: {
+  GridNavigation
+},
+setup() {
+  const user = ref(null);
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (currentUser) => {
+    user.value = currentUser;
+  });
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error.message);
     }
+  }
+
+  return { user, handleLogout };
+}
 }
 </script>
-    
+
+<style scoped>
+.container {
+display: flex;
+flex-direction: column;
+min-height: 100vh;
+}
+
+nav {
+position: sticky;
+top: 0;
+background-color: #f8f9fa;
+padding: 10px;
+}
+
+main {
+flex: 1;
+padding: 20px;
+}
+
+footer {
+position: sticky;
+bottom: 0;
+background-color: #f8f9fa;
+padding: 10px;
+text-align: center;
+}
+</style>
---
+++ src/components/MainGrid.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid">
+        asdasdas
+      <router-link to="/view1">View 1</router-link>
+      <router-link to="/view2">View 2</router-link>
+      <router-link to="/view3">View 3</router-link>
+      <!-- Add more links as needed -->
+    </div>
+  
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </template>
+  
+  <script>
+  export default {
+    name: 'GridNavigation'
+  }
+  </script>
+  
+  <style scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); /* Adjust as needed */
+    gap: 10px;
+  }
+  
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
